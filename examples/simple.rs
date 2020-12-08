ea874ffadfd56838077dc4b42ca33d2abd785ccc
Stefan Müller zzeroo <co@zzeroo.com>
2020-12-08 08:37:16 +0100

    Remove the feature `use_extern_macros`
    This feature has been stable since 1.30.0 and no longer requires an attribute to enable

--- examples/simple.rs
+++ examples/simple.rs
@@ -19,8 +19,6 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#![feature(use_extern_macros)]
-
 extern crate gtk;
 extern crate mg;
 extern crate mg_settings;
